3eb577f347265eb85425e4f12b490a902215c06a
zmudzin <istredd@gmail.com>
2022-11-25 21:45:51 +0100

    test

---
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -0,0 +1,47 @@
+package pl.coderslab;
+
+import java.sql.*;
+
+public class DbUtil {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306?useSSL=false&characterEncoding=utf8&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "coderslab";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+    public static void insert(Connection connect, String query, String... params) {
+        try ( PreparedStatement statement = connect.prepareStatement(query)) {
+            for (int i = 0; i < params.length; i++) {
+                statement.setString(i + 1, params[i]);
+            }
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void printData(Connection conn, String query, String... columnNames) throws SQLException {
+        try (PreparedStatement statement = conn.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                for (String columnName : columnNames) {
+                    System.out.println(resultSet.getString(columnName));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static final String DELETE_QUERY = "DELETE FROM tableName where id = ?";
+
+    public static void remove(Connection conn, String tableName, int id) {
+        try (PreparedStatement statement =
+                     conn.prepareStatement(DELETE_QUERY.replace("tableName", tableName))) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
