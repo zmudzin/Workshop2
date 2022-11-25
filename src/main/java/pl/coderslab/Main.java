077afa911393d57746d74d05f2c4b3297bb8042f
zmudzin <istredd@gmail.com>
2022-11-25 22:14:04 +0100

    wstęp

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,10 @@
 package pl.coderslab;
 
+import com.mysql.cj.util.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world222!");
+
     }
 }
---
+++ src/main/java/pl/coderslab/User.java
@@ -0,0 +1,46 @@
+package pl.coderslab;
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String name) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
